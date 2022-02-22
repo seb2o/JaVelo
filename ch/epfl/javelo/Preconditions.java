97e8994b8f65b367974a4acc7c1f4256acaf54a2
Edgar <edgar.gonzalez-gascou@epfl.ch>
2022-02-22 11:56:46 +0100

    precond
    Preconditions

---
+++ ch/epfl/javelo/Preconditions.java
@@ -0,0 +1,13 @@
+package ch.epfl.javelo;
+
+public final class Preconditions {
+    private Preconditions(){
+
+    }
+    public static void checkArgument(boolean shouldBeTrue){
+        if(!shouldBeTrue){
+            throw new IllegalArgumentException();
+        }
+
+    }
+}
