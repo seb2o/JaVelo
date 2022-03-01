55da8c4926a35d2fbce26b64b4634b53eabf3191
BTagazok <87731987+BTagazok@users.noreply.github.com>
2022-03-01 14:55:54 +0100

    fin classe q28_4

--- ch/epfl/javelo/Q28_4.java
+++ ch/epfl/javelo/Q28_4.java
@@ -1,19 +1,21 @@
 package ch.epfl.javelo;
 
 public final class Q28_4 {
-    private  Q28_4(){
-    }
-    public static int ofInt(int i){
-        int q = (int)-Math.scalb(Bits.extractUnsigned(i,0,1),28);
-        for (int j = 1; j < 30; j++) {
-            q += (int)Math.scalb(Bits.extractUnsigned(i,j,1),28-j);
-        }
-        return q;
-    }
-    /**public static double asDouble(int q28_4){
 
-    }
-    public static float asFloat(int q28_4){
+    private  Q28_4(){}
 
-    }*/
+    public static int ofInt(int i) {
+        Preconditions.checkArgument(i<=Math.pow(2,28)-1);
+        return i << 4;
+    }
+
+    public static double asDouble(int i){
+        return Math.scalb(i,-4);
+    }
+
+    public static float asFloat(int i){
+        return Math.scalb(i,-4);
+    }
+
+
 }
