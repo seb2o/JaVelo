95034d60e74b2442e5baf5a119f883ec2dad6628
BTagazok <87731987+BTagazok@users.noreply.github.com>
2022-03-03 21:33:42 +0100

    ajout attribute set

--- ch/epfl/javelo/Main.java
+++ ch/epfl/javelo/Main.java
@@ -6,7 +6,11 @@ import ch.epfl.javelo.data.AttributeSet;
 public class Main
 {
     public static void main(String[] args) {
-        AttributeSet att = new AttributeSet(1);
-        System.out.println(att.contains(Attribute.HIGHWAY_SERVICE));
+//        AttributeSet att = new AttributeSet(1);
+//        System.out.println(att.contains(Attribute.HIGHWAY_SERVICE));
+
+        System.out.println();
+
     }
+
 }
--- test/ch/epfl/javelo/BitsTest.java
+++ test/ch/epfl/javelo/BitsTest.java
@@ -2,6 +2,7 @@ package ch.epfl.javelo;
 
 import org.junit.jupiter.api.Test;
 
+import static ch.epfl.test.TestRandomizer.newRandom;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BitsTest {
@@ -17,25 +18,25 @@ public class BitsTest {
     }
     @Test
     public void signedExtractionTestLastBit(){
-        var actual = Bits.extractSigned(1,0,1);
+        var actual = Bits.extractSigned(1,0,2);
         var expected = 1;
         assertEquals(expected, actual);
     }
 
     @Test
     public void signedExtractionTestWholeChain(){
-        for (int i = Integer.MIN_VALUE; i <= Integer.MAX_VALUE ; i+=(int)Math.pow(2,10)) {
-            var actual = Bits.extractSigned(i,0,32);
-            assertEquals(i,actual);
-        }
+        int i = newRandom().nextInt();
+        var actual = Bits.extractSigned(i,0,32);
+        assertEquals(i,actual);
+
     }
 
     @Test
     public void signedExtractionTestClassicIntervals(){
         System.out.println(Integer.toBinaryString(1<<10));
-        var actual = Bits.extractSigned(1<<10,9,2);
+        var actual = Bits.extractSigned(1<<10,9,3);
         System.out.println(Integer.toBinaryString(actual));
-        var expected = 0b010000000000;
+        var expected = 0b010;
         assertEquals(expected,actual);
     }
 }
