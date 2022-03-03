08df6f081a3248d7d23768b4acd63382fae642b9
BTagazok <87731987+BTagazok@users.noreply.github.com>
2022-03-03 15:01:39 +0100

    fin sampled + commentaire jusqu'a la classe bits

--- ch/epfl/javelo/Bits.java
+++ ch/epfl/javelo/Bits.java
@@ -1,9 +1,20 @@
 package ch.epfl.javelo;
 
+/**
+ * @author Edgar Gonzales (328095)
+ */
 public final class Bits {
-    private Bits(){
-    }
 
+    private Bits(){}
+
+    /**
+     * extrait du vecteur de 32 bits value la plage de length bits commençant au bit d'index start
+     * interprètée comme une valeur signée en complément à deux, IllegalArgumentException si la plage est invalide
+     * @param value le récéptacle du vecteur de bits, sous forme d'int
+     * @param start le début de la plage à extraire, supérieure à 0
+     * @param length la longueure de la plage à extraire, inférieure a 31 lorsque sommé a l'indice de début de la plage
+     * @return la valeure signée du vecteur de bit extrait
+     */
     public static int extractSigned(int value, int start, int length){
         Preconditions.checkArgument(start >= 0 & length >= 0 & start+length <= 31);
         value = value << 32 - (start + length);
@@ -11,9 +22,16 @@ public final class Bits {
         return value;
     }
 
+    /**
+     * extrait du vecteur de 32 bits value la plage de length bits commençant au bit d'index start
+     * IllegalArgumentException si la plage est invalide ou si le vecteur de bit à extraire est de taille 32
+     * @param value le récéptacle du vecteur de bits, sous forme d'int
+     * @param start le début de la plage à extraire, supérieure à 0
+     * @param length la longueure de la plage à extraire, inférieure a 31 lorsque sommée a l'indice de début de la plage
+     * @return la valeure toujours positive du vecteur de bit extrait
+     */
     public static int extractUnsigned(int value, int start, int length){
-        Preconditions.checkArgument(start >= 0 & start+length <= 31 & length > 0 & length != 32); //TODO "size" dans le pdf = length ?
-        value = value << 32 - (start + length);                                                                               //TODO ca parle aussi de complément à deux..
+        Preconditions.checkArgument(start >= 0 & start+length <= 31 & length > 0 & length != 32);
         value = value >>> 32 - length;
         return value;
     }
--- ch/epfl/javelo/projection/PointWebMercator.java
+++ ch/epfl/javelo/projection/PointWebMercator.java
@@ -16,10 +16,22 @@ public record PointWebMercator(double x, double y) {
         Preconditions.checkArgument(x >=0 & x <= 1 & y >=0 & y <= 1 );
     }
 
+    /**
+     * construit un point de coordonnées exprimées dans le système mercator zoomé
+     * @param zoomLevel le niveau de zoom du système mercator
+     * @param x la coordonnée x
+     * @param y la coordonné y
+     * @return un point de coordonnées exprimées dans un système mercator zoomé
+     */
     public static PointWebMercator of(int zoomLevel, double x, double y){
         return new PointWebMercator(Math.scalb(x,-(8+zoomLevel)),Math.scalb(y,-(8+zoomLevel)));
     }
 
+    /**
+     * construit un point exprimé dans le système mercator à partir d'un point ch1903
+     * @param pointCh le point exprimé dans le système ch1903
+     * @return le même point de coordonnées exprimées dans le système webMercator
+     */
     public static PointWebMercator ofPointCh(PointCh pointCh){
         double lon = Ch1903.lon(pointCh.e(),pointCh.n());
         double lat = Ch1903.lat(pointCh.e(),pointCh.n());
@@ -28,22 +40,44 @@ public record PointWebMercator(double x, double y) {
         return new PointWebMercator(x,y);
     }
 
+    /**
+     * retourne la coordonnée du point x exprimée dans un niveau de zoom donné
+     * @param zoomLevel le niveau de zoom donné
+     * @return la coordonée x du point dans le système webMercator au niveu de zoom donné
+     */
     public double xAtZoomLevel(int zoomLevel){
         return Math.scalb(x,8+zoomLevel);
     }
 
+    /**
+     * retourne la coordonnée du point y exprimée dans un niveau de zoom donné
+     * @param zoomLevel le niveau de zoom donné
+     * @return la coordonée y du point dans le système webMercator au niveu de zoom donné
+     */
     public double yAtZoomLevel(int zoomLevel){
         return Math.scalb(y,8+zoomLevel);
     }
 
+    /**
+     * retourne la longitude dans le système WGS de l'instance
+     * @return la longitude dans le système WGS de l'instance
+     */
     public double lon(){
         return WebMercator.lon(x);
     }
 
+    /**
+     * retourne la latitude dans le système WGS de l'instance
+     * @return la latitude dans le système WGS de l'instance
+     */
     public double lat(){
         return WebMercator.lat(y);
     }
 
+    /**
+     * retourne l'instance du point sous forme de point en coordonnées ch1903
+     * @return l'instance du point sous forme de point en coordonnées ch1903
+     */
     public PointCh toPointCh(){
         double e = Ch1903.e(lon(),lat());
         double n = Ch1903.n(lon(),lat());
