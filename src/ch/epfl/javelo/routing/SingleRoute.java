06b9813be2eea0999c0775360ff4621ac30f4a35
seb <menbuas69@gmail.com>
2022-03-22 09:12:51 +0100

    fix elevationAt

--- src/ch/epfl/javelo/routing/SingleRoute.java
+++ src/ch/epfl/javelo/routing/SingleRoute.java
@@ -9,14 +9,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public final class SingleRoute implements Route{
-    public final List<Edge> edges;
+    private final List<Edge> edges;
     private final double[] lengthList;
 
     public SingleRoute(List<Edge> edges){
         Preconditions.checkArgument(!edges.isEmpty());
         this.edges = List.copyOf(edges); //todo copie profonde ? Nécessite méthode .clone pour Edge car pas immuable.
         this.lengthList = new double[edges.size() + 1];
-        this.lengthList[0] = 1;
+        this.lengthList[0] = 0;
 
         for (int i = 1; i < this.lengthList.length; i++) {
             this.lengthList[i] = this.lengthList[i-1] + edges.get(i-1).length();
@@ -86,9 +86,11 @@ public final class SingleRoute implements Route{
 
     @Override
     public double elevationAt(double position) {
+        System.out.println("computed lenght : "+ length());
         position = Math2.clamp(0,position,length());
         double relativePosition;
         int index = Arrays.binarySearch(lengthList, position);
+        System.out.println("index " + index);
         if(index >= 0){ //Si l'index correspond à un noeud.
             relativePosition = 0;
             if(index == lengthList.length - 1){ // Si c'est le dernier noeud, on prend la position à la fin de la dernière arête
@@ -96,10 +98,11 @@ public final class SingleRoute implements Route{
             }
         }
         else{
-            index = -(index + 1); //Donne l'index de l'arête correspondante.
+            index = - index - 2 ; //Donne l'index de l'arête correspondante.
+            System.out.println(" shift :  "+ lengthList[index]);
             relativePosition = position - lengthList[index];
         }
-
+        System.out.println(relativePosition);
         return edges.get(index).elevationAt(relativePosition);
     }
 
--- test/ch/epfl/javelo/ElevationProfileComputerTest.java
+++ test/ch/epfl/javelo/ElevationProfileComputerTest.java
@@ -1,6 +1,7 @@
 package ch.epfl.javelo;
 
 import ch.epfl.javelo.projection.PointCh;
+import ch.epfl.javelo.projection.SwissBounds;
 import ch.epfl.javelo.routing.Edge;
 import ch.epfl.javelo.routing.SingleRoute;
 import org.junit.jupiter.api.Test;
@@ -8,15 +9,18 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.DoubleUnaryOperator;
+import java.util.function.Function;
 
 public class ElevationProfileComputerTest {
 
     @Test
     public void elevationProfileTest() {
-        DoubleUnaryOperator profile = null;
+        DoubleUnaryOperator profile = Functions.sampled(new float[]{1,2,3,4,20,6,8,9,10},10);
+        System.out.println("manual compuation : "+profile.applyAsDouble(4.5));
         List<Edge> edges = new ArrayList<Edge>(){};
-        edges.add(new Edge(0,0,new PointCh(0,0),new PointCh(0,0),0, null));
+        edges.add(new Edge(0,0,new PointCh(SwissBounds.MIN_E,SwissBounds.MIN_N),new PointCh(SwissBounds.MIN_E+50,SwissBounds.MIN_N+50),10, profile));
         SingleRoute route = new SingleRoute(edges);
+        System.out.println("actual computation : "+route.elevationAt(4.5));
     }
 
 
