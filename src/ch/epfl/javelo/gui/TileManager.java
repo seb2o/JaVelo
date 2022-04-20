9611f3235e649644cd18ac83d404b779c9d4b03f
seb <menbuas69@gmail.com>
2022-04-20 12:26:22 +0200

    tileManager avec png probleme

--- src/ch/epfl/javelo/gui/TileManager.java
+++ src/ch/epfl/javelo/gui/TileManager.java
@@ -7,6 +7,7 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -17,29 +18,34 @@ import java.util.Map;
  */
 public final class TileManager {
 
-    private final Path tilesPath;
+    private final Path cachePath;
     private final String servName;
     private final LinkedHashMap<TileId, Image> cache = new LinkedHashMap<>(100, 0.75f, true);
 
     public TileManager(Path tilePath, String servName) {
         this.servName = servName;
-        this.tilesPath = tilePath;
+        this.cachePath = tilePath;
     }
 
     public Image getImageOf(TileId tile)  {
+
+
+        if (!TileId.isValid(tile)) {
+            System.out.println("invalid tile");
+            return null;
+        }
+
 
         //si la tuile est stockée en cache, on la retourne et la fonction se termine
         Image tileImage = cache.get(tile);
         if (tileImage!=null) return tileImage;
 
 
-        Path zoomDirectory = Path.of(String.valueOf(tile.zoomLevel()));
-        Path xDirectory = Path.of(String.valueOf(tile.x()));
-        Path imagePath = tilesPath
-                .resolve(zoomDirectory)
-                .resolve(xDirectory)
-                .resolve(String.valueOf(tile.y()));
-
+        Path zoomDirectory = cachePath
+                .resolve(Path.of(String.valueOf(tile.zoomLevel())));
+        Path xDirectory = zoomDirectory
+                .resolve(Path.of(String.valueOf(tile.x())));
+        Path imagePath = xDirectory.resolve(String.valueOf(tile.y())+".png");
         //si la tuile est stockée sur le disque, on la stocke en cache et on la retourne
         try (FileInputStream f = new FileInputStream(imagePath.toString())) {
             return cacheAndReturnTile(tile,f);
@@ -57,20 +63,14 @@ public final class TileManager {
                 tileImage = cacheAndReturnTile(tile,i);
 
                 if (Files.exists(xDirectory)) {
-                    //lire l'image, la transférer à un output stream qui crée
-                    //le fichier image dans imagePath
+                    writeStream(i,imagePath);
                 }
 
-                else if (Files.exists(zoomDirectory)) {
-                    //créer le dossier du zoomLevel
-                    //lire l'image, la transférer à un output stream qui crée
-                    //le fichier image dans imagePath
-                }
                 else {
-                    //creer
+                    Files.createDirectories(xDirectory);
+                    writeStream(i,imagePath);
                 }
 
-                i.close();
                 return tileImage;
 
             } catch (IOException ex) { //si erreur, alors souci de programmation
@@ -81,9 +81,6 @@ public final class TileManager {
 
 
 
-        //todo check if zoom level directory is created, if it is then check if x coord directory is created, if it is, add the Image
-        //todo if not, create the missing elements and add the image
-        //todo then add it to the memory cache, normally it will remove the less used entry
 
     }
 
@@ -100,10 +97,23 @@ public final class TileManager {
         return cacheAndReturnTile(id, new Image(f));
     }
 
+
+    private void writeStream(InputStream inputStream, Path path) throws IOException {
+        FileOutputStream o = new FileOutputStream(path.toString());
+        inputStream.transferTo(o);
+        inputStream.close();
+        o.close();
+    }
+
     record TileId(int zoomLevel, int x, int y) {
+
         public static boolean isValid(int zoomLevel, int xIndex, int yIndex){
             int numberOfTiles = 1<<zoomLevel*2;
             return (0<=zoomLevel && 0<=xIndex && 0<=yIndex && zoomLevel<=19 && xIndex<numberOfTiles && yIndex<numberOfTiles);
+        }
+
+        public static boolean isValid(TileId tileId) {
+            return isValid(tileId.zoomLevel(), tileId.x(), tileId.y());
         }
     }
 
---
+++ test/ch/epfl/javelo/gui/TileManagerTest.java
@@ -0,0 +1,15 @@
+package ch.epfl.javelo.gui;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+
+public class TileManagerTest {
+
+    @Test
+    public void getImageOf(){
+        TileManager m = new TileManager(Path.of("tiles"),"https://tile.openstreetmap.org");
+        System.out.println(m.getImageOf(new TileManager.TileId(1,0,0)));
+    }
+
+}
