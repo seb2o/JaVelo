26e214d0df4d7be9530b7f5254b6547d40365960
Edgar <edgar.gonzalez-gascou@epfl.ch>
2022-04-30 14:47:01 +0200

    amelio p9

--- src/ch/epfl/javelo/gui/RouteBean.java
+++ src/ch/epfl/javelo/gui/RouteBean.java
@@ -7,8 +7,10 @@ import javafx.beans.property.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
+import javafx.scene.image.Image;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 public final class RouteBean {
@@ -17,19 +19,20 @@ public final class RouteBean {
     private DoubleProperty highlightedPosition;
     private SimpleObjectProperty<ElevationProfile> elevationProfile; //Todo : idem
     private RouteComputer routeComputer;
+    private LinkedHashMap<WaypointPair, Route> cache = new LinkedHashMap<>(100, 0.75f, true);
 
     public RouteBean(RouteComputer routeComputer){
         this.routeComputer = routeComputer;
-        this.highlightedPosition = new SimpleDoubleProperty();
+        this.highlightedPosition = new SimpleDoubleProperty(Double.NaN);
         this.route = new SimpleObjectProperty<>();
         this.elevationProfile = new SimpleObjectProperty<>();
 
         this.waypoints = FXCollections.observableArrayList(
-                new Waypoint(new PointCh(2532697, 1152350), 159049),
+                new Waypoint(new PointCh(2532697, 1152350), 159049), //Todo : mettre dans des ctes ?
                 new Waypoint(new PointCh(2538659, 1154350), 117669));
 
         if(waypoints.size() >= 2){
-            this.route.set(computeRouteBetween(0, waypoints.size()));
+            this.route.set(computeMultiRoute());
             //elevationProfile = new SimpleObjectProperty<>(ElevationProfileComputer.elevationProfile(route.get(),5.0));
         }
         else{
@@ -39,27 +42,32 @@ public final class RouteBean {
         waypoints.addListener((ListChangeListener<Waypoint>) c -> {
             c.next();
             if(c.wasAdded()){
-                route.set(computeRouteBetween(c.getFrom() - 1, c.getFrom()));
+                route.set(computeMultiRoute());
                 //elevationProfile = new SimpleObjectProperty<>(ElevationProfileComputer.elevationProfile(route.get(),5.0));
             }
             if(c.wasRemoved()){
-                route.set(computeRouteBetween(1,2));
+                route.set(computeMultiRoute());
             }
-
-            //Todo : wasremoved etc;
-            //Todo : cache ? mais ca me parait inutile;
         });
 
-
     }
-    private Route computeRouteBetween(int start, int end){
-        Preconditions.checkArgument(start < end); //Todo :peut-être dispensable
+
+
+    private MultiRoute computeMultiRoute(){
         List<Route> routeList = new ArrayList<>();
-        for (int i = 0; i < waypoints().size() - 1; i++) { //Todo : remettre tsart et end ?
-            routeList.add(routeComputer.bestRouteBetween(waypoints.get(i).closestNodeId(), waypoints.get(i+1).closestNodeId()));
+        for(int index = 0; index < waypoints.size() - 1; index++){
+            routeList.add(computeRouteBetween(waypoints().get(index), waypoints().get(index+1)));
         }
         return new MultiRoute(routeList);
+    }
 
+    private Route computeRouteBetween(Waypoint w1, Waypoint w2){
+        WaypointPair wp = new WaypointPair(w1,w2);
+        Route route = cache.get(wp);
+            if(route != null){
+                return route;
+            }
+        return routeComputer.bestRouteBetween(w1.closestNodeId(), w2.closestNodeId());
     }
 
     public ReadOnlyObjectProperty<Route> routeProperty(){
@@ -80,5 +88,33 @@ public final class RouteBean {
 
     public void setHighlightedPosition(double highlightedPosition) {
         this.highlightedPosition.set(highlightedPosition);
+    }
+
+    private class WaypointPair{
+        private Waypoint w1;
+        private Waypoint w2;
+        private WaypointPair(Waypoint w1, Waypoint w2){
+            this.w1 = w1;
+            this.w2 = w2;
+        }
+
+        public Waypoint get1(){
+            return w1;
+        }
+
+        public  Waypoint get2(){
+            return w2;
+        }
+
+        @Override
+        public boolean equals(Object object){
+            if(object instanceof  WaypointPair){
+                Waypoint ow1 = ((WaypointPair) object).get1();
+                Waypoint ow2 = ((WaypointPair) object).get2();
+                return (ow1.equals(w1) && ow2.equals(w2)) || (ow1.equals(w2) && ow2.equals(w1));
+                //Todo : peut etre pas mettre le deuxieme termes après le ||;
+            }
+            return false;
+        }
     }
 }
--- src/ch/epfl/javelo/gui/RouteManager.java
+++ src/ch/epfl/javelo/gui/RouteManager.java
@@ -4,6 +4,7 @@ import ch.epfl.javelo.projection.PointCh;
 import ch.epfl.javelo.projection.PointWebMercator;
 import ch.epfl.javelo.routing.Edge;
 import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polyline;
@@ -15,24 +16,23 @@ import java.util.function.Consumer;
 public final class RouteManager {
 
     private Pane pane;
-    private MapViewParameters mapViewParameters;
+    private ReadOnlyObjectProperty<MapViewParameters> mapViewParameters;
     private RouteBean routeBean;
+    private Circle highlight;
 
     public RouteManager(RouteBean routeBean, ReadOnlyObjectProperty<MapViewParameters> mapViewParametersProperty, Consumer<String> consumer){
         this.pane = new Pane();
         this.routeBean = routeBean;
-        this.mapViewParameters = mapViewParametersProperty.get();
+        this.mapViewParameters = mapViewParametersProperty;
         Polyline polyline = new Polyline();
-        Circle highlight = new Circle(5);
+        highlight = new Circle(5);
         polyline.setId("route");
         highlight.setId("highlight");
-        pane.getChildren().addAll(polyline,highlight);
+        pane.getChildren().addAll(polyline, highlight);
         pane.setPickOnBounds(false);
         polyline.getPoints().addAll(buildPointList());
+        updateHighlightPosition();
 
-        PointCh highlightPch = routeBean.route().pointAt(routeBean.getHighlitedPosition());
-        highlight.setLayoutX(mapViewParameters.viewX(PointWebMercator.ofPointCh(highlightPch)));
-        highlight.setLayoutY(mapViewParameters.viewY(PointWebMercator.ofPointCh(highlightPch)));
 
         mapViewParametersProperty.addListener( (observable, oldValue, newValue) -> {
             double oldX = oldValue.originX();
@@ -45,27 +45,56 @@ public final class RouteManager {
             if((oldX != newX || oldY != newY) && oldZoom == newZoom){
                 polyline.setLayoutX(polyline.getLayoutX() + oldX - newX);
                 polyline.setLayoutY(polyline.getLayoutY() + oldY - newY);
-                highlight.setLayoutX(highlight.getLayoutX() + oldX - newX);
-                highlight.setLayoutY(highlight.getLayoutY() + oldY - newY);
+                if(!Double.isNaN(routeBean.getHighlitedPosition())){
+                    highlight.setLayoutX(highlight.getLayoutX() + oldX - newX);
+                    highlight.setLayoutY(highlight.getLayoutY() + oldY - newY);
+                }
+            }
+
+            if(oldZoom != newZoom){
+                polyline.getPoints().setAll(buildPointList());
+                polyline.setLayoutX(0);
+                polyline.setLayoutY(0); //Pas facile à trouver cette astuce!
+                updateHighlightPosition();
             }
         });
 
         routeBean.routeProperty().addListener(((observable, oldValue, newValue) -> {
             polyline.getPoints().setAll(buildPointList());
+            polyline.setLayoutX(0);
+            polyline.setLayoutY(0);
         }));
+
+        highlight.setOnMousePressed(e ->{
+            if(e.isStillSincePress()){
+                System.out.println("Un point de passage est déjà présent à cet endroit !\n");
+            }
+            //Todo : faire un truc pour creer un waypoint au bon endroit
+        });
+
     }
     public Pane pane(){
         return pane;
     }
 
-    public List<Double> buildPointList(){
+    private List<Double> buildPointList(){
         List<Double> pointListTemp = new ArrayList<>();
         for (Edge edge: routeBean.route().edges()) {
             PointCh pch = edge.fromPoint();
             PointWebMercator pwb = PointWebMercator.ofPointCh(pch);
-            pointListTemp.add(mapViewParameters.viewX(pwb));
-            pointListTemp.add(mapViewParameters.viewY(pwb));
+            pointListTemp.add(mapViewParameters.get().viewX(pwb));
+            pointListTemp.add(mapViewParameters.get().viewY(pwb));
         }
         return pointListTemp;
     }
+    private void updateHighlightPosition(){
+        double highlightPos = routeBean.getHighlitedPosition();
+        if (Double.isNaN(highlightPos)) {
+            return;
+        }
+        PointCh highlightPch = routeBean.route().pointAt(highlightPos);
+        highlight.setLayoutX(mapViewParameters.get().viewX(PointWebMercator.ofPointCh(highlightPch)));
+        highlight.setLayoutY(mapViewParameters.get().viewY(PointWebMercator.ofPointCh(highlightPch)));
+    }
+
 }
